419a60930f257c3d9fad9bb8922079bcdf6d566b
EveryGameSPlay <repsbro@gmail.com>
2026-03-18 20:56:49 +0300

    yougile: базовые типы

--- src/lib/yougile/types/auth.ts
+++ src/lib/yougile/types/auth.ts
@@ -1,4 +1,13 @@
-export type LoginBody = {
-    login: string;
-    password: string;
-};
+export type Login = string;
+
+export type Password = string;
+
+/**
+ * Этот ключ выдается при логине и сохраняется в куках браузера.
+ */
+export type UserCookieKey = string;
+
+/**
+ * Ключ компании. Можно получить по API.
+ */
+export type CompanyKey = string;
